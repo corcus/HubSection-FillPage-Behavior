c26eb973560a1c4df7e0138a781ab191d4b90678
Panagiotis Bakos <panagiotisbakos@gmail.com>
2016-10-20 15:26:13 +0300

    Add the behavior project

---
+++ HubSectionFillPageBehavior/HubSectionFillPageBehavior/DeviceTypeHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.System.Profile;
+
+namespace HubSectionFillPageBehavior
+{
+    public static class DeviceTypeHelper
+    {
+        public static DeviceFormFactorType GetDeviceFormFactorType()
+        {
+            switch (AnalyticsInfo.VersionInfo.DeviceFamily)
+            {
+                case "Windows.Mobile":
+                    return DeviceFormFactorType.Phone;
+                case "Windows.Desktop":
+                    return DeviceFormFactorType.Desktop;
+                case "Windows.Universal":
+                    return DeviceFormFactorType.IoT;
+                case "Windows.Team":
+                    return DeviceFormFactorType.SurfaceHub;
+                default:
+                    return DeviceFormFactorType.Other;
+            }
+        }
+    }
+
+    public enum DeviceFormFactorType
+    {
+        Phone,
+        Desktop,
+        IoT,
+        SurfaceHub,
+        Other
+    }
+}
---
+++ HubSectionFillPageBehavior/HubSectionFillPageBehavior/HubSectionFillPageBehavior.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+using Microsoft.Xaml.Interactivity;
+
+
+namespace HubSectionFillPageBehavior
+{
+    public class HubSectionFillPageBehavior : DependencyObject, IBehavior
+    {
+        public DependencyObject AssociatedObject { get; private set; }
+
+        public void Attach(DependencyObject associatedObject)
+        {
+            if (this.AssociatedObject != null)
+            {
+                throw new InvalidOperationException("Cannot assign to the same behavior twice.");
+            }
+            else if (associatedObject.GetType() != typeof(Hub))
+            {
+                throw new InvalidOperationException("Can only assign to Hub");
+            }
+
+            this.AssociatedObject = associatedObject;
+
+            if (DeviceTypeHelper.GetDeviceFormFactorType() == DeviceFormFactorType.Phone)
+            {
+                (AssociatedObject as Hub).SizeChanged += HubSectionFillPageBehavior_SizeChanged;
+                (AssociatedObject as Hub).Loaded += HubSectionFillPageBehavior_Loaded;
+            }
+        }
+
+        private void HubSectionFillPageBehavior_Loaded(object sender, RoutedEventArgs e)
+        {
+            Hub associatedHub = this.AssociatedObject as Hub;
+            associatedHub.ApplyTemplate();
+            var scrollViewer = getScrollViewer(associatedHub);
+            if (scrollViewer != null)
+            {
+                scrollViewer.HorizontalSnapPointsType = SnapPointsType.MandatorySingle;
+            }
+        }
+
+        private void HubSectionFillPageBehavior_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            SetHubSectionsWidth();
+        }
+
+        private void SetHubSectionsWidth()
+        {
+            Hub associatedHub = this.AssociatedObject as Hub;
+            for (int i = 0; i < associatedHub.Sections.Count - 1; i++)
+            {
+                associatedHub.Sections[i].Width = associatedHub.ActualWidth - 24; // leave some space for the next hubsection to be visible
+            }
+            associatedHub.Sections[associatedHub.Sections.Count - 1].Width = associatedHub.ActualWidth;
+
+        }
+
+        private ScrollViewer getScrollViewer(DependencyObject o)
+        {
+            // Return the DependencyObject if it is a ScrollViewer
+            if (o is ScrollViewer)
+            {
+                return o as ScrollViewer;
+            }
+
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(o); i++)
+            {
+                var child = VisualTreeHelper.GetChild(o, i);
+
+                var result = getScrollViewer(child);
+                if (result == null)
+                {
+                    continue;
+                }
+                else
+                {
+                    return result;
+                }
+            }
+            return null;
+        }
+
+
+        public void Detach()
+        {
+            if (DeviceTypeHelper.GetDeviceFormFactorType() == DeviceFormFactorType.Phone)
+            {
+                (AssociatedObject as Hub).SizeChanged -= HubSectionFillPageBehavior_SizeChanged;
+                (AssociatedObject as Hub).Loaded -= HubSectionFillPageBehavior_Loaded;
+            }
+            this.AssociatedObject = null;
+        }
+    }
+}
